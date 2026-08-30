db816a34cb208522d70dab75e801cc3d7e91bf89
rlagmlaks00 <gmlaksgmlaks@naver.com>
2026-08-30 15:19:10 +0900

    DLQ 처리 - 간단한 예제 코드

--- src/main/java/com/example/rabbitmq/messaging/OrderConsumer.java
+++ src/main/java/com/example/rabbitmq/messaging/OrderConsumer.java
@@ -11,7 +11,7 @@ public class OrderConsumer {
     @RabbitListener(queues = RabbitMQConfig.QUEUE_NAME)
     public void handleOrderCreated(OrderCreatedEvent event) {
         System.out.println("주문 처리 시작: " + event.orderId());
-//      throw new RuntimeException("일부러 실패시켜서 DLQ 테스트");
+        throw new RuntimeException("일부러 실패시켜서 DLQ 테스트");
         // 실제 비즈니스 로직 (재고 차감, 알림 발송 등)
     }
 }
